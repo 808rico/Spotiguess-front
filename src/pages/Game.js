19d5d720557a9550a8b4b749ee2ec1f29e0faf84
Aymeric GRANIER <shopidev34@gmail.com>
2023-12-14 22:42:59 -0600

    game start

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -26,14 +26,12 @@ function Game() {
     const location = useLocation();
     const { type, input, songUris } = location.state;
     console.log(songUris);
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
-    const accessToken = localStorage.getItem('access_token')
-    spotifyApi.setAccessToken(accessToken);
-    const isDesktopOrLaptop = useMediaQuery({ minWidth: 700 });
+    const [accessToken, setAccessToken] = useState(localStorage.getItem('access_token'))
     const [isFirstPlayClicked, setIsFirstPlayClicked] = useState(false);
     const [currentTrack, setCurrentTrack] = useState(null);
     const [showPopup, setShowPopup] = useState(false);
+    const [player, setPlayer] = useState(null);
+    console.log(accessToken);
 
     const handleNextTrack = () => {
         player && player.nextTrack();
@@ -45,101 +43,48 @@ function Game() {
 
     const handlePlayClick = () => {
         setIsFirstPlayClicked(true);
-        playTracks(songUris);
+        //playTracks(songUris);
     };
 
 
-    useEffect(() => {
-        if (!accessToken) return
-        spotifyApi.setAccessToken(accessToken)
-    }, [accessToken])
-
-    const [player, setPlayer] = useState(undefined);
-
-    function transferPlaybackToDevice(deviceId) {
-        fetch('https://api.spotify.com/v1/me/player', {
-            method: 'PUT',
-            headers: {
-                'Authorization': `Bearer ${accessToken}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ device_ids: [deviceId], play: false })
-        })
-            .then(response => {
-                if (response.ok) {
-                    console.log('Playback transferred to device');
-                } else {
-                    console.error('Error transferring playback', response);
-                }
-            })
-            .catch(error => console.error('Error transferring playback', error));
-    }
-
-
-
 
     useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "https://sdk.scdn.co/spotify-player.js";
-        script.async = true;
-        document.body.appendChild(script);
+        if (accessToken && !player) {
+            const script = document.createElement('script');
+            script.src = 'https://sdk.scdn.co/spotify-player.js';
+            script.async = true;
+            document.body.appendChild(script);
 
-        window.onSpotifyWebPlaybackSDKReady = () => {
-            const player = new window.Spotify.Player({
-                name: 'Web Playback SDK',
-                getOAuthToken: cb => { cb(accessToken); },
-                volume: 0.5
-            });
+            window.onSpotifyWebPlaybackSDKReady = () => {
 
-            setPlayer(player);
-
-            player.addListener('ready', ({ device_id }) => {
-                console.log('Ready with Device ID', device_id);
-                transferPlaybackToDevice(device_id);
-            });
-
-            player.addListener('not_ready', ({ device_id }) => {
-                console.log('Device ID has gone offline', device_id);
-            });
-
-            player.addListener('player_state_changed', ({ device_id }) => {
-                player.getCurrentState().then(state => {
-                    if (!state) {
-                        console.error('User is not playing music through the Web Playback SDK');
-                        return;
-                    }
-
-                    var current_track = state.track_window.current_track;
-                    //var next_track = state.track_window.next_tracks[0];
-                    setCurrentTrack(current_track)
-
+                const player = new window.Spotify.Player({
+                    name: 'Your Web Player',
+                    getOAuthToken: cb => { cb(accessToken); }
                 });
 
-                console.log('player_state_changed', device_id);
-            });
+                // Connect to the player!
+                player.connect();
+                setPlayer(player);
 
-            player.connect();
-        };
-    }, [accessToken]);
+                // Event listeners can be set here
+                player.addListener('ready', ({ device_id }) => {
+                    console.log('Ready with Device ID', device_id);
+                });
 
-    function playTracks(uris) {
-        fetch('https://api.spotify.com/v1/me/player/play', {
-            method: 'PUT',
-            headers: {
-                'Authorization': `Bearer ${accessToken}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ uris: uris })
-        })
-            .then(response => {
-                if (response.ok) {
-                    console.log('Playback started');
-                } else {
-                    console.error('Error starting playback', response);
-                }
-            })
-            .catch(error => console.error('Error starting playback', error));
-    }
+                // Handle player state changes, etc.
+            };
+        }
+    }, [accessToken, player]);
+
+
+
+    
+
+
+
+    
+
+    
 
     return (
 
