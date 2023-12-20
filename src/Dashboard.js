ed2e6249b9eaaf70cf1e8a3bc4e39be14396ad61
Aymeric GRANIER <shopidev34@gmail.com>
2023-12-19 21:55:56 -0600

    bug prod

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,11 +1,11 @@
-import { BrowserRouter as  Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import AIGenerated from './pages/AIGenerated';
+//import { BrowserRouter as  Route, Routes } from 'react-router-dom';
+//import Home from './pages/Home';
+//import AIGenerated from './pages/AIGenerated';
 
 //import YourLikedSongs from './pages/YourLikedSongs';
 //import Playlist from './pages/Playlist';
 //import Artist from './pages/Artist';
-import Game from './pages/Game';
+//import Game from './pages/Game';
 
 /*
 <Route path="/liked-songs" component={YourLikedSongs} />
