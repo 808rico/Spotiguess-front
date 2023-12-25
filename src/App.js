12fb06924111be00352bdad6ebace3adfc0c8329
Aymeric GRANIER <shopidev34@gmail.com>
2023-12-25 17:16:36 -0600

    login

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
-import Login from './Login.js'
-import React from 'react';
-import Dashboard from './Dashboard.js'
-import useAuth from './UseAuth.js';
+import React, { useEffect, useState } from 'react';
+import { Spin } from 'antd';
+//import { useNavigate } from 'react-router-dom';
+import useAuth from './UseAuth'; // Assurez-vous que le chemin d'importation est correct
+import Dashboard from './Dashboard';
+import Login from './Login';
 import './app.css'
 
 
 
 function App() {
-
   const code = new URLSearchParams(window.location.search).get('code');
-  console.log(code)
-  const accessToken =  useAuth(code);
-console.log(accessToken)
-  const script = document.createElement('script');
-  script.src = 'https://sdk.scdn.co/spotify-player.js';
-  script.async = true;
-  document.body.appendChild(script);
-  
-  console.log('app')
+  //const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  useAuth(code);
+  const accessToken = localStorage.getItem('access_token');
+
+  useEffect(() => {
+    if (!code && !accessToken) {
+      window.location.assign('https://spotiguess.com'); // Redirection hors de l'app React
+    }
+    if (accessToken) {
+      setLoading(false);
+    }
+  }, [code, accessToken]);
+
+  if (loading) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <Spin size="large" />
+      </div>
+    );
+  }
 
   if (accessToken) {
-    return(
-
-        <Dashboard accessToken={accessToken}/>
-      )
-  }
-
-  else{
-    return(
-        <Login />
-)
+    return <Dashboard accessToken={accessToken} />;
+  } else {
+    return <Login />;
   }
 }
+
 
 
 export default App;
