dde5d7cae23a7bafeccd5d82bfdb261cbea180b2
Aymeric GRANIER <shopidev34@gmail.com>
2023-12-31 12:29:34 -0600

    bug player solved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Spin } from 'antd';
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 import useAuth from './UseAuth'; // Assurez-vous que le chemin d'importation est correct
 import Dashboard from './Dashboard';
-import Login from './Login';
+//import Login from './Login';
 import './app.css'
 
 
@@ -17,26 +17,26 @@ function App() {
 
   useEffect(() => {
     if (!code && !accessToken) {
-      //window.location.assign('https://spotiguess.com'); // Redirection hors de l'app React
+      window.location.assign('https://spotiguess.com'); // Redirection hors de l'app React
     }
     if (accessToken) {
-      //setLoading(false);
+      setLoading(false);
     }
   }, [code, accessToken]);
-/*
+
   if (loading) {
     return (
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
         <Spin size="large" />
       </div>
     );
-  }*/
+  }
 
   if (accessToken) {
     return <Dashboard accessToken={accessToken} />;
   } else {
-    return <Login />;
-    //window.location.assign('https://spotiguess.com')
+    //return <Login />;
+    window.location.assign('https://spotiguess.com')
     
   }
 }
