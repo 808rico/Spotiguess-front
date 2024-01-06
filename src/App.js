4ae0b0943c6b1a555a553267077d691ddd2668b8
Aymeric GRANIER <shopidev34@gmail.com>
2024-01-05 23:01:33 -0600

    handle premium + loader login

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import useAuth from './UseAuth';
 import Dashboard from './Dashboard';
 import Login from './Login';
+import { Spin } from 'antd'; // Importer le composant Spin d'Ant Design
 import './app.css';
+import SpotifyWebApi from 'spotify-web-api-node';
+import { message } from 'antd';
 
 function App() {
-  const code = new URLSearchParams(window.location.search).get('code');
-  const accessToken = useAuth(code); // utilisez le hook pour obtenir l'access token
+  const [isLoading, setIsLoading] = useState(true);
+  const [isPremium, setIsPremium] = useState(false);
+  const accessToken = useAuth(new URLSearchParams(window.location.search).get('code'));
+  const spotifyApi = useRef(new SpotifyWebApi());
 
   useEffect(() => {
-    // Supprimez la vérification du code car useAuth s'en occupe
-    if (!accessToken) {
-      // Si pas de token, rediriger vers Login
-      // Vous pouvez implémenter une logique pour vérifier l'expiration du token ici
+    if (accessToken) {
+      spotifyApi.current.setAccessToken(accessToken);
+      spotifyApi.current.getMe().then(data => {
+
+        if (data.body.product !== 'premium') {
+          message.error('You need a premium Spotify account to use this app');
+        }
+
+        setIsPremium(data.body.product === 'premium');
+        setIsLoading(false);
+      }).catch((err) => {
+        message.error('An error occured while trying to get your Spotify account type', err);
+        setIsPremium(false);
+        setIsLoading(false);
+      });
+    } else {
+      setIsLoading(false);
     }
   }, [accessToken]);
 
-  // Gestion du rendu conditionnel
-  if (!accessToken) {
-    return <Login />;
-  } else {
-    return <Dashboard accessToken={accessToken} />;
+  if (isLoading) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Spin size="large" />
+    </div>
+    
+    );
   }
+
+  return accessToken && isPremium ? <Dashboard accessToken={accessToken} /> : <Login />;
 }
 
 export default App;
